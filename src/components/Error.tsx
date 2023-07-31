37800076e2769abbc5da98a4e4d814ce14e8cec6
Lasseignejk <jaye914@hotmail.com>
2023-07-31 17:47:22 -0400

    Updated Error component

--- src/components/Error.tsx
+++ src/components/Error.tsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from "react";
 
 const Error = () => {
-  return (
-    <div>Error</div>
-  )
-}
+	return (
+		<div>
+			<h1>Oh no!</h1>
+			<h3>
+				It looks like we can't find that page. Click{" "}
+				<a href="/">here</a>
+				to go back to the home page.
+			</h3>
+		</div>
+	);
+};
 
-export default Error
+export default Error;
